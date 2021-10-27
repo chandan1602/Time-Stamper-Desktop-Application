4d94379b3a7bbd68cb7e36b8c81a23da96798144
chandan1602 <cbansal82@gmail.com>
2021-10-27 18:32:18 +0530

    Added support for 32 bit architecture

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-TIME STAMPER APPLICATION
+**TIME STAMPER APPLICATION**
 
+##Requirements:
 OPERATING SYSTEM : Windows 7/8/10
-Features:
-	1. Automatic Date Stamper
-	2. Custom Date Stamper
-	3. Two Additional Fields Provided to Stamp
-	4. Text Available in 3 Colors
-	5. Custom Font Size Available.
-	6. Progress Bar for user ease.
+
+##Features:
+1. Automatic Date Stamper
+2. Custom Date Stamper
+3. Two Additional Fields Provided to Stamp
+4. Text Available in 3 Colors
+5. Custom Font Size Available.
+6. Progress Bar for user ease.
+7. Added Support for 32 and 64 bit architectures.
--- pipInstall.py
+++ pipInstall.py
@@ -4,8 +4,15 @@ import sys
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-def build():
-    subprocess.check_call(r"py -m PyInstaller --noconsole --name Stamper --onefile F:\TimeStamperPython\stamper.py")
+def uninstall(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "uninstall", package])
 
+def build():
+    subprocess.check_call(r"py -m PyInstaller --noconsole --name Stamper32 --onefile F:\TimeStamperPython\stamper.py")
+
+def build32():
+    subprocess.check_call(r"py -3.7-32 -m PyInstaller --noconsole --name Stamper32 --onefile F:\TimeStamperPython\stamper.py")
+
+# uninstall("pyinstaller")
 # install("pyinstaller")
 build()
